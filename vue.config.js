f673fa300fa1c91a8715330efd8039c864724272
unknown <abhayrockks@gmail.com>
2025-01-27 23:19:52 +0530

    project setup

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    open: true // This will open the browser automatically when the server starts
+  }
 })
